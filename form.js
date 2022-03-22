326ecfe9fb0539ab6f9f726cc586e16a77306309
kamilaw <you@example.com>
2022-03-22 12:55:39 +0000

    contact form + modal

--- form.js
+++ form.js
@@ -1,7 +1,6 @@
 
 
 
-
 const form = document.querySelector("form");
 
 const input = form.querySelector("input");
@@ -112,22 +111,58 @@ inputName.addEventListener("input", () => {
 });
 
 inputPrenome.addEventListener("input", () => {
-    validateInputs();  // chcemy zeby rozwy czarwnoy kwadrat zniknal jak ktros wpisze nowe value
+    validateInputs();  
 });
 
 inputEmail.addEventListener("input", () => {
-    validateInputs();  // chcemy zeby rozwy czarwnoy kwadrat zniknal jak ktros wpisze nowe value
+    validateInputs();  
 });
 inputTelephone.addEventListener("input", () => {
-    validateInputs();  // chcemy zeby rozwy czarwnoy kwadrat zniknal jak ktros wpisze nowe value
+    validateInputs();  
 
 });
 
 
 inputMessage.addEventListener("input", () => {
-    validateInputs();  // chcemy zeby rozwy czarwnoy kwadrat zniknal jak ktros wpisze nowe value
+    validateInputs();  
 
 });
 
 
+///==================MODAL================================
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
